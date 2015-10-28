b87d253e9bb87c09924c03ce7e47b051163431a1
Erik Söderberg <Zednosius@gmail.com>
2015-10-28 12:15:25 +0100

    Added more tables that will be useful to have and separate file with table handles

---
+++ DB_HANDLES.py
@@ -0,0 +1,7 @@
+REPLAYS     = "replays"
+MATCH_DATA  = "match_data"
+TEAMS       = "teams"
+TAGS        = "tags"
+NOTES       = "notes"
+GROUPS      = "groups"
+GROUP_MEMBERS = "group_members"
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python2
 import psycopg2
 import sys
+import DB_HANDLES
 
-def main():
+def initdb():
     #Define our connection string
     conn_string = "host='localhost' port='5433' dbname='postgres' user='postgres' password='alpha'"
  
@@ -16,16 +17,31 @@ def main():
     cursor = conn.cursor()
 
     #ID; FILENAME; NAME; MAP;
-    cursor.execute("CREATE TABLE replays (id serial PRIMARY KEY,filename varchar(64),name text,map text);")
+    cursor.execute("CREATE TABLE replays (id serial PRIMARY KEY,filename varchar(64),name text,map text,pdate date;ptime time;)")
+    
     #ID(fk); TEAM; PLAYERCOUNT; GOALS;  (information about a team in a match)
-    cursor.execute("CREATE TABLE match_data (id integer REFERENCES replays ON UPDATE CASCADE ON DELETE CASCADE, team integer, playercount integer, goals integer, saves integer)")
+    cursor.execute("CREATE TABLE match_data (id integer REFERENCES replays ON DELETE CASCADE, team integer, playercount integer, goals integer, saves integer)")
+    
     #ID(fk);TEAM; PLAYERNAME; GOALS; SAVES; (information about a player in a match)
-    cursor.execute("CREATE TABLE teams (id integer REFERENCES replays ON UPDATE CASCADE ON DELETE CASCADE, team integer, playername text, goals integer, saves integer);")
-    #ID(fk);TAGNAME;TIMESTAMP; (User created tags for a match)
-    cursor.execute("CREATE TABLE tags (id integer REFERENCES replays ON UPDATE CASCADE ON DELETE CASCADE, tagname text, timestamp integer);")
+    cursor.execute("CREATE TABLE teams (id integer REFERENCES replays ON DELETE CASCADE, team integer, playername text, goals integer, saves integer);")
+    
+    #ID(fk);TAGNAME;TIMESTAMP; (User created time tags for a match)
+    cursor.execute("CREATE TABLE tags (id integer REFERENCES replays ON DELETE CASCADE, tagname text, timestamp integer);")
+    
+    #ID(fk); NOTE; (Other notes that doesn't have a timestamp)
+    cursor.execute("CREATE TABLE notes (id integer REFERENCES replays ON DELETE CASCADE, note text;)")
+    
+    #ID;NAME; (For grouping a set of replays together) 
+    cursor.execute("CREATE TABLE groups (g_id serial PRIMARY KEY; name text);")
+
+    #Many-to-Many relation
+    cursor.execute("CREATE TABLE group_members (g_id integer REFERENCES group ON DELETE CASCADE, id integer REFERENCES replays ON DELETE CASCADE;)")
 
     conn.commit()
     print "Success!"
-    
+
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 1:
+        initdb()
+    else:
+        if 
