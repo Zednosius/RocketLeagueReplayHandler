93e8bd4c0d1991c6eb12988afabd532732bbd860
Erik Söderberg <Zednosius@gmail.com>
2015-10-28 17:24:58 +0100

    Added basic insertions for all tables

--- db_manager.py
+++ db_manager.py
@@ -20,26 +20,40 @@ class DB_Manager():
         self.conn = sqlite3.connect("rocketleague.db")
         self.conn.execute("PRAGMA foreign_keys = ON")
 
-    def add_replay(self, filename, name, mapname):
+    def add_replay(self, filename, name, mapname, datetime):
         with self.conn:
-            self.conn.execute("INSERT INTO replays VALUES (?, ?, ?);", filename, name, mapname)
-            dprint("Inserted %s %s %s into replays ", filename,name,mapname)
+            self.conn.execute("INSERT INTO replays (filename,name,map,datetime) VALUES (?, ?, ?, ?);", filename, name, mapname,datetime)
+            self.dprint("Inserted %s %s %s into replays ", filename,name,mapname,datetime)
 
     def add_team(self,ID,teamNum,player_name,goals=None,saves=None):
         with self.conn:
-            self.conn.execute("INSERT INTO teams VALUES (? ? ? ? ?);",ID,teamNum,player_name,goals,saves)
-        
-    def add_tag(self):
-        pass
-        
-    def add_match_data(self):
-        pass
+            self.conn.execute("INSERT INTO teams VALUES (?, ?, ?, ?, ?);",ID,teamNum,player_name,goals,saves)
+            self.dprint("Inserted %s %s %s %s %s into teams",ID,teamNum,player_name,goals,saves)
 
-    def add_note(self):
-        pass
+    def add_tag(self,ID,tagname,timestamp):
+        with self.conn:
+            self.conn.execute("INSERT INTO tags VALUES (?, ?, ?);",ID,tagname,timestamp)
+            self.dprint("Inserted %s %s %s into tags",ID,tagname,timestamp)
 
-    def add_group(self):
-        pass
+    def add_match_data(self,ID,teamNum,goals,saves):
+        with self.conn:
+            self.conn.execute("INSERT INTO match_data VALUES (?, ?, ?, ?);",ID,teamNum,goals,saves)
+            self.dprint("Inserted %s %s %s %s into match_data", ID,teamNum,goals,saves)
+
+    def add_note(self, ID, note):
+        with self.conn:
+            self.conn.execute("INSERT INTO notes VALUES (?, ?);",ID,note)
+            self.dprint("Inserted %s %s into notes",ID,note)
+
+    def add_group(self, group_name):
+        with self.conn:
+            self.conn.execute("INSERT INTO groups (name) VALUES (?);", group_name)
+            self.dprint("Inserted %s into groups",group_name)
+
+    def add_replay_to_group(self, ID, gID):
+        with self.conn:
+            self.conn.execute("INSERT INTO group_members VALUES (?, ?);")
+            self.dprint("Inserted %s %s into group_members",ID,gID)
 
     def close(self):   
         self.conn.close()
