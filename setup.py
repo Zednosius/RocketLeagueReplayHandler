7c3aefb155bef204049b07fc3194ec8f2b8ead63
Erik Söderberg <Zednosius@gmail.com>
2015-10-28 16:27:18 +0100

    Changed from psycopg2 to sqlite3
    Realized that sqlite is a much better fit because people won't have to
    install an entire database manager for it.

--- setup.py
+++ setup.py
@@ -4,49 +4,42 @@
 #See LICENSE for more information
 
 
-import psycopg2
+import sqlite3
 import sys
 import DB_HANDLES
 
+
 def initdb():
-    #Define our connection string
-    conn_string = "host='localhost' port='5433' dbname='postgres' user='postgres' password='alpha'"
- 
-    # print the connection string we will use to connect
-    print "Connecting to database\n ->%s" % (conn_string)
- 
-    # get a connection, if a connect cannot be made an exception will be raised here
-    conn = psycopg2.connect(conn_string)
- 
-    # conn.cursor will return a cursor object, you can use this cursor to perform queries
-    cursor = conn.cursor()
 
-    #ID; FILENAME; NAME; MAP;
-    cursor.execute("CREATE TABLE replays (id serial PRIMARY KEY,filename varchar(64),name text,map text,pdate date;ptime time;)")
-    
-    #ID(fk); TEAM; PLAYERCOUNT; GOALS;  (information about a team in a match)
-    cursor.execute("CREATE TABLE match_data (id integer REFERENCES replays ON DELETE CASCADE, team integer, playercount integer, goals integer, saves integer)")
-    
-    #ID(fk);TEAM; PLAYERNAME; GOALS; SAVES; (information about a player in a match)
-    cursor.execute("CREATE TABLE teams (id integer REFERENCES replays ON DELETE CASCADE, team integer, playername text, goals integer, saves integer);")
-    
-    #ID(fk);TAGNAME;TIMESTAMP; (User created time tags for a match)
-    cursor.execute("CREATE TABLE tags (id integer REFERENCES replays ON DELETE CASCADE, tagname text, timestamp integer);")
-    
-    #ID(fk); NOTE; (Other notes that doesn't have a timestamp)
-    cursor.execute("CREATE TABLE notes (id integer REFERENCES replays ON DELETE CASCADE, note text;)")
-    
-    #ID;NAME; (For grouping a set of replays together) 
-    cursor.execute("CREATE TABLE groups (g_id serial PRIMARY KEY; name text);")
+  # print the connection string we will use to connect
+    print "Connecting to database"
+    conn = sqlite3.connect("rocketleague.db")
+    conn.execute("PRAGMA foreign_keys = ON")
+    with conn:      
 
-    #Many-to-Many relation
-    cursor.execute("CREATE TABLE group_members (g_id integer REFERENCES group ON DELETE CASCADE, id integer REFERENCES replays ON DELETE CASCADE;)")
+        #ID; FILENAME; NAME; MAP;
+        conn.execute("CREATE TABLE replays (id INTEGER PRIMARY KEY,filename varchar(64),name text,map text,datetime text);")
+        
+        #ID(fk); TEAM; PLAYERCOUNT; GOALS;  (information about a team in a match)
+        conn.execute("CREATE TABLE match_data (id integer REFERENCES replays ON DELETE CASCADE, team integer, playercount integer, goals integer, saves integer);")
+        
+        #ID(fk);TEAM; PLAYERNAME; GOALS; SAVES; (information about a player in a match)
+        conn.execute("CREATE TABLE teams (id integer REFERENCES replays ON DELETE CASCADE, team integer, playername text, goals integer, saves integer);")
+        
+        #ID(fk);TAGNAME;TIMESTAMP; (User created time tags for a match)
+        conn.execute("CREATE TABLE tags (id integer REFERENCES replays ON DELETE CASCADE, tagname text, timestamp integer);")
+        
+        #ID(fk); NOTE; (Other notes that doesn't have a timestamp)
+        conn.execute("CREATE TABLE notes (id integer REFERENCES replays ON DELETE CASCADE, note text);")
+        
+        #ID;NAME; (For grouping a set of replays together) 
+        conn.execute("CREATE TABLE groups (g_id integer PRIMARY KEY, name text);")
 
-    conn.commit()
-    print "Success!"
+        #Many-to-Many relation
+        conn.execute("CREATE TABLE group_members (g_id integer REFERENCES groups ON DELETE CASCADE, id integer REFERENCES replays ON DELETE CASCADE);")
+
+        print "Success!"
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         initdb()
-    else:
-        if 
