10ae9ea6f4d09c82a10862b7fc920935b0562c0d
Erik Söderberg <Zednosius@gmail.com>
2015-11-01 23:47:02 +0100

    Added popup for editing row value in table

--- gui/Popups.py
+++ gui/Popups.py
@@ -6,7 +6,7 @@ import Tkinter as tk
 import ttk
 import tkFont
 from db_manager import *
-
+import re
 
 class TagPopup(tk.Toplevel):
     def __init__(self,parent=None,**kw):
@@ -51,6 +51,53 @@ class TagPopup(tk.Toplevel):
         self.tagname.focus_set()
     def close(self):
         pass
+
+class TableRowEditPopup(tk.Toplevel):
+    def __init__(self,parent=None,**kw):
+        self.row_values = kw.pop("row_values",[])
+        wroot_x,wroot_y = kw.pop("winfo_rootc",(0,0))
+        self.done_callback = kw.pop("done_callback",None)
+        tk.Toplevel.__init__(self,parent,**kw)
+        self.notif = tk.StringVar()
+
+
+        self.grab_set()
+        label_col=0
+        entry_col=1
+        self.entries = []
+        print self.row_values
+
+        for (i,txt) in enumerate(["Player Name","Team","Goals","Saves","Shots","Assists","Score"]):
+            print i,txt
+            tk.Label(self,text=txt).grid(row=i,column=label_col,sticky="we")
+            self.entries.append(tk.Entry(self))
+            self.entries[i].grid(row=i,column=entry_col,sticky="we")
+            if self.row_values and i < len(self.row_values)-1: #Skip the ID which is the first elem in row_values
+                self.entries[i].insert("end",self.row_values[i+1])
+
+        tk.Button(self,text="Done",command=self.done).grid(row=i+1,column=0,columnspan=2,sticky="we")
+        tk.Label(self,textvariable=self.notif).grid(row=i+2,column=0,columnspan=2,sticky="we")
+        
+        self.entries[0].focus_set()    
+        self.geometry("+%d+%d" % (wroot_x+50,
+                                  wroot_y+50))
+    def valid(self):
+        print self.entries[0].get(),self.entries[1].get()
+        valid = len(self.entries[0].get()) > 0
+        valid &= re.match("^\d$",self.entries[1].get()) != None
+        return valid
+
+    def set_done_callback(self,callback):
+        self.done_callback = callback
+
+    def done(self):
+        if not self.valid(): 
+            self.notif.set("Invalid entries")
+            return
+        self.final_values = (None,)+tuple(map(lambda ent: ent.get() , self.entries))
+        if self.done_callback:
+            self.done_callback(self.final_values)
+        self.destroy()
 
 class FilterPopup(tk.Toplevel):
 
