59717a2de25ae158f936260ea38c2f86260c8511
Erik Söderberg <Zednosius@gmail.com>
2015-11-01 23:49:37 +0100

    Modified EditFrame to allow changing of table row values

--- gui/ReplayEditFrame.py
+++ gui/ReplayEditFrame.py
@@ -12,6 +12,7 @@ import replay_parser
 import re
 import shutil
 import rl_paths
+import Popups
 
 class ReplayEditFrame(tk.Frame):
     def __init__(self,parent,**kw):
@@ -20,6 +21,8 @@ class ReplayEditFrame(tk.Frame):
         self.notif_text = tk.StringVar()
         self.notif = tk.Label(self,textvariable=self.notif_text,fg="#DD0000")
         self.notif.grid(row=0,column=1,sticky="we")
+        self.progress_bar = ttk.Progressbar(self)
+        self.progress_bar.grid(row=0,column=2,sticky="we")
         label_row = 1
         entry_row = 2
 
@@ -41,19 +44,18 @@ class ReplayEditFrame(tk.Frame):
         style = ttk.Style(self)
         style.configure('Treeview', rowheight=40)
         self.table['show'] = 'headings'
-        self.table.heading("#1", text="Player")
-        self.table.heading("#2", text="Team"  )
-        self.table.heading("#3", text="Goals" )
-        self.table.heading("#4", text="Saves" )
-        self.table.heading("#5", text="Shots" )
-        self.table.heading("#6", text="Assists" )
-        self.table.heading("#7", text="Score" )
+        self.table_headings = ["Player","Team","Goals","Saves","Shots","Assists","Score"]
+        for (idx,txt) in enumerate(self.table_headings):
+            self.table.heading("#"+str(idx+1), text=txt)
+
         for col in self.allcols:
             self.table.column(col,anchor='center',minwidth=75,width=75)
         self.table.column('#1',anchor='center',minwidth=100,width=100)
 
         self.table.grid(row=entry_row+1,column=0,columnspan=4,sticky="NEWS")
-        
+        self.table.bind("<<TreeviewSelect>>",self.edit_row)
+        self.add_row_button = tk.Button(self,text="Add row",command=self.add_row)
+        self.add_row_button.grid(row=entry_row+2,column=0,columnspan=4,sticky="NWE")
         
 
         self.grid_columnconfigure(0,weight=1)
@@ -81,19 +83,18 @@ class ReplayEditFrame(tk.Frame):
 
             for k,v in names_goals.items():
                 self.values.append((None,k,int(names_goals[k][1]),names_goals[k][0]))
-        self.name.delete(0,"end")
-        self.mapname.delete(0,"end")
-        self.date.delete(0,"end")
+        
+        self.clear()
 
         self.name.insert(0,"Replay "+self.headers[1])
         self.mapname.insert(0,data['header']['MapName'])
         self.date.insert(0,self.headers[1])
         self.table_insert_values()
+        self.progress_bar.values=0
 
     def table_insert_values(self):
         """Inserts all values in self.values into the table"""
-        self.table.delete(*self.table.get_children())
-
+    
         for values in self.values:
             self.table.insert("", "end",
              values=values[1:],
@@ -104,21 +105,36 @@ class ReplayEditFrame(tk.Frame):
 
         self.table.tag_configure('orange' , background='#FF7F00',font=self.mFont)
         self.table.tag_configure('blue', background='#82CFFD',font=self.mFont)
+        self.table.tag_configure('new_row', background='#00ff00',font=self.mFont)
+
+    def clear(self):
+        self.table.delete(*self.table.get_children())
+        self.name.delete(0,"end")
+        self.mapname.delete(0,"end")
+        self.date.delete(0,"end")
 
     def create_entry(self):
         if not self.valid(): return
         replay_name = self.name.get() 
         map_name    = self.mapname.get()
         date        = self.date.get()
+        steps = float(2+len(self.values))
+        print "Creating entry"
         try:
             with DB_Manager() as dmann:
                 c = dmann.add_replay(filename=self.headers[0],name=replay_name, mapname=map_name, date_time=date)
+                self.progress_bar.step(100/steps)
                 idx = c.lastrowid
                 self.replay_entry = (idx,self.headers[0],replay_name,map_name,date)
+                print "replay entry: ",self.replay_entry
                 for values in self.values:
                     print idx,values
+                    
                     dmann.add_team(idx,*(values[1:]))
+                    self.progress_bar.step(100/steps)
                 dmann.add_note(idx,"")
+                self.progress_bar.step(100/steps)
+            self.notif_text.set("Replay added")
         except sqlite3.IntegrityError, e:
             self.notif_text.set("ERROR: COULD NOT CREATE ENTRY\n"+str(e))
 
@@ -127,9 +143,37 @@ class ReplayEditFrame(tk.Frame):
         if not re.match("\d{4}-\d{2}-\d{2} \d{2}:\d{2}",date):
             self.notif_text.set("Date format must be YYYY-MM-DD hh:mm")
             return False
-
+        for values in self.values:
+            if values[1] == None or values[2] == None:
+                self.notif_text.set("Name and Team must be entered")
+                return False
 
         return True
 
-    def edit_row(self, rowindex):
-        pass
+    def edit_row(self,event):
+        print event
+        print event.widget
+        selection = self.table.selection()[0]
+        row_index = self.table.index(selection)
+        vals = []
+
+        if row_index < len(self.values):
+            vals = self.values[row_index]
+
+        popup = Popups.TableRowEditPopup(self,row_values=vals,winfo_rootc=(self.winfo_rootx(),self.winfo_rooty()))
+        popup.set_done_callback(lambda vals : self.replace_row(selection,vals))
+
+    def add_row(self):
+        self.table.insert("", "end",
+             values= ("None",)*( len(self.values[0])-1 if self.values[0] else 7),
+             tags=("new_row"))
+
+    def replace_row(self,row_selection,vals):
+        for (idx,heading) in enumerate(self.allcols):
+            self.table.set(row_selection,heading,vals[idx+1])
+        idx = self.table.index(row_selection)
+        if idx < len(self.values):
+            self.values[idx] = vals
+        else:
+            self.values.append(vals) 
+        print vals
