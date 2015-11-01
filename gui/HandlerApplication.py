f99bd1883155b0b4c7b78cab57a3cffb26bdc38d
Erik Söderberg <Zednosius@gmail.com>
2015-11-01 18:24:57 +0100

    Removed methods moved to rl_paths. Added ability to track replays

--- gui/HandlerApplication.py
+++ gui/HandlerApplication.py
@@ -13,23 +13,14 @@ from Popups import *
 from ReplayInfoFrame import *
 from ReplayList import *
 from ReplayEditFrame import *
-from os.path import expanduser
-import errno
+
+import rl_paths
 
 
-
-def make_dirs():
-    try:
-        os.makedirs(expanduser("~")+ReplayManager._default_path+"\\tracked")
-        os.makedirs(expanduser("~")+ReplayManager._default_path+"\\untracked")
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
 
 class ReplayManager(tk.Frame):
-    _default_path = "\Documents\My Games\Rocket League\TAGame\Demos"
     def __init__(self,parent, **kw):
-        make_dirs()
+        rl_paths.make_dirs()
         tk.Frame.__init__(self, parent, **kw)
         n = ttk.Notebook(self)
         f1 = tk.Frame(n)   # first page, which would get widgets gridded into it
@@ -52,11 +43,11 @@ class ReplayManager(tk.Frame):
         f  = tk.Frame(frame)
         f2 = tk.Frame(frame)
         scrollbar = tk.Scrollbar(f, orient=tk.VERTICAL)
-        self.replay_list = ReplayList(f,yscrollcommand=scrollbar.set)
-        self.replay_list.bind("<MouseWheel>",lambda event : self.replay_list.yview("scroll",-event.delta/120,"units"))
-        scrollbar.config(command=self.replay_list.yview)
+        self.tracked_replays = ReplayList(f,yscrollcommand=scrollbar.set)
+        self.tracked_replays.bind("<MouseWheel>",lambda event : self.tracked_replays.yview("scroll",-event.delta/120,"units"))
+        scrollbar.config(command=self.tracked_replays.yview)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        self.replay_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        self.tracked_replays.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
         self.fetch_replays()
 
         
@@ -68,7 +59,7 @@ class ReplayManager(tk.Frame):
         self.staged_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
         
 
-        # self.replay_list.grid(row=1,column=0,sticky="NSWE")
+        # self.tracked_replays.grid(row=1,column=0,sticky="NSWE")
         # self.staged_list.grid(row=1,column=2,sticky="NSWE")
         f.grid(row=1,column=0,sticky="NSWE")
         f2.grid(row=1,column=2,sticky="NSWE")
@@ -78,8 +69,8 @@ class ReplayManager(tk.Frame):
 
         self.info.grid(row=1,column=1,rowspan=2,sticky="NS")
 
-        self.replay_list.link(self.staged_list)
-        self.staged_list.link(self.replay_list)
+        self.tracked_replays.link(self.staged_list)
+        self.staged_list.link(self.tracked_replays)
 
         frame.grid_columnconfigure(0,weight=1)
         frame.grid_columnconfigure(2,weight=1)
@@ -104,6 +95,8 @@ class ReplayManager(tk.Frame):
         self.edit_frame = ReplayEditFrame(frame)
         self.edit_frame.grid(row=1,column=1,sticky="NWSE")
         tk.Button(frame,text="rescan",command=self.scan_and_fetch_untracked).grid(row=2,column=0,sticky="WE")
+        self.create_entry_button = tk.Button(frame,text="Track", command=self.track_selected_file)
+        self.create_entry_button.grid(row=2,column=1,sticky="WE")
 
         frame.grid_rowconfigure(1,weight=1)
         frame.grid_columnconfigure(0,weight=1)
@@ -112,14 +105,19 @@ class ReplayManager(tk.Frame):
 
     def fetch_replays(self,replayfilters={},tagfilters={},playerfilters={},groupfilters={}):
         with DB_Manager() as mann:
-            replays = mann.filter_replays(replayfilters,tagfilters,playerfilters,groupfilters)
+            if replayfilters  or tagfilters or playerfilters or groupfilters:
+                print "dicts: ",replayfilters,tagfilters,playerfilters,groupfilters
+                replays = mann.filter_replays(replayfilters,tagfilters,playerfilters,groupfilters)
+            else:
+                replays = mann.get_all("replays","date_time desc")
 
         for replay in replays:
-            self.replay_list.insert("end",replay[2],replay)
+            self.tracked_replays.insert("end",replay[2],replay)
 
     def scan_and_fetch_untracked(self):
-        p = expanduser("~")+self._default_path
-        untracked = p+"\\untracked"
+        p = rl_paths.demo_folder()
+        untracked = rl_paths.untracked_folder()
+        backup = rl_paths.backup_folder()
         if self.untracked_replays.size() > 0:
             self.untracked_replays.delete(0,self.untracked_replays.size())
 
@@ -133,8 +131,9 @@ class ReplayManager(tk.Frame):
                 if os.path.isfile(fullpath) and not dmann.replay_exists(filename):
                     print "%s was not in database"%(filename)
                     try:
-                        shutil.copy2(fullpath,untracked+"\\"+f)
-                        os.remove(fullpath)
+                        shutil.copy2(fullpath,untracked+"\\"+f) #Copy to untracked folder
+                        shutil.copy2(fullpath,backup+"\\"+f) #Copy to backup folder
+                        os.remove(fullpath) #Remove old copy
                         print "Moved %s to untracked"
                     except Exception, e:
                         print "Error during file handling"
@@ -145,13 +144,25 @@ class ReplayManager(tk.Frame):
                     self.staged_list.insert("end",var[2],var)
 
         l = os.listdir(untracked)
-        l.sort(reverse=True,key=lambda x:os.path.getctime(untracked+"\\"+x))
+        l.sort(reverse=True,key=lambda x:os.path.getmtime(untracked+"\\"+x))
+        print l
         for f in l:
             filename = os.path.splitext(f)[0]
             fullpath = untracked+"\\"+f
-            time = datetime.datetime.fromtimestamp(os.path.getmtime(fullpath)).strftime("%Y-%m-%d-%H:%M")
+            time = datetime.datetime.fromtimestamp(os.path.getmtime(fullpath)).strftime("%Y-%m-%d %H:%M")
             self.untracked_replays.insert("end","Replay "+str(time),(filename,time,fullpath))
                     
+    def track_selected_file(self):
+        self.edit_frame.create_entry()
+        shutil.move(rl_paths.untracked_folder(self.edit_frame.headers[0]),rl_paths.tracked_folder(self.edit_frame.headers[0]))
+        print self.edit_frame.replay_entry
+        
+        self.untracked_replays.delete_selected()
+        self.tracked_replays.insert(0,self.edit_frame.replay_entry[2],self.edit_frame.replay_entry)
+
+
+        # self.tracked_replays.insert(0,self.edit_frame.values[2],self.edit_frame.values)
+        # self.untracked_replays.delete()
 
     def replay_display_edit(self,variables):
         self.edit_frame.display_new(list(variables))
