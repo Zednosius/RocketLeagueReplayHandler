fc5466c3f909772d2d4f87b38b17f3b1e9c377a0
Erik Söderberg <Zednosius@gmail.com>
2015-11-01 18:22:40 +0100

    Added methods for creating an entry in the database

--- gui/ReplayEditFrame.py
+++ gui/ReplayEditFrame.py
@@ -7,9 +7,11 @@ import Tkinter as tk
 import ttk
 import tkFont
 from db_manager import *
-
+import sqlite3
 import replay_parser
-
+import re
+import shutil
+import rl_paths
 
 class ReplayEditFrame(tk.Frame):
     def __init__(self,parent,**kw):
@@ -27,8 +29,8 @@ class ReplayEditFrame(tk.Frame):
         
         self.name = tk.Entry(self)
         self.name.grid(row=entry_row,column=0,sticky="we")
-        self.map = tk.Entry(self)
-        self.map.grid(row=entry_row,column=1,sticky="we")
+        self.mapname = tk.Entry(self)
+        self.mapname.grid(row=entry_row,column=1,sticky="we")
         self.date= tk.Entry(self)
         self.date.grid(row=entry_row,column=2,sticky="we")
 
@@ -52,6 +54,8 @@ class ReplayEditFrame(tk.Frame):
 
         self.table.grid(row=entry_row+1,column=0,columnspan=4,sticky="NEWS")
         
+        
+
         self.grid_columnconfigure(0,weight=1)
         self.grid_columnconfigure(1,weight=1)
         self.grid_columnconfigure(2,weight=1)
@@ -61,7 +65,7 @@ class ReplayEditFrame(tk.Frame):
         data = replay_parser.ReplayParser().parse(variables[2])
         self.headers = variables
         self.values = []
-
+        print self.headers
         #New replay
         if 'PlayerStats' in data['header'].keys():
             self.notif_text.set("There might be missing data, doublecheck")
@@ -78,11 +82,11 @@ class ReplayEditFrame(tk.Frame):
             for k,v in names_goals.items():
                 self.values.append((None,k,int(names_goals[k][1]),names_goals[k][0]))
         self.name.delete(0,"end")
-        self.map.delete(0,"end")
+        self.mapname.delete(0,"end")
         self.date.delete(0,"end")
 
         self.name.insert(0,"Replay "+self.headers[1])
-        self.map.insert(0,data['header']['MapName'])
+        self.mapname.insert(0,data['header']['MapName'])
         self.date.insert(0,self.headers[1])
         self.table_insert_values()
 
@@ -93,14 +97,39 @@ class ReplayEditFrame(tk.Frame):
         for values in self.values:
             self.table.insert("", "end",
              values=values[1:],
-             tags=("red" if int(values[2]) == 1 else "blue"))
+             tags=("orange" if int(values[2]) == 1 else "blue"))
 
             if(self.table.column("#1","width") < self.mFont.measure(values[1])): #Adjust table column size if needed
                 self.table.column("#1",width=int(self.mFont.measure(values[1])*1.2))
 
-        self.table.tag_configure('red' , background='#FF7F00',font=self.mFont)
+        self.table.tag_configure('orange' , background='#FF7F00',font=self.mFont)
         self.table.tag_configure('blue', background='#82CFFD',font=self.mFont)
 
+    def create_entry(self):
+        if not self.valid(): return
+        replay_name = self.name.get() 
+        map_name    = self.mapname.get()
+        date        = self.date.get()
+        try:
+            with DB_Manager() as dmann:
+                c = dmann.add_replay(filename=self.headers[0],name=replay_name, mapname=map_name, date_time=date)
+                idx = c.lastrowid
+                self.replay_entry = (idx,self.headers[0],replay_name,map_name,date)
+                for values in self.values:
+                    print idx,values
+                    dmann.add_team(idx,*(values[1:]))
+                dmann.add_note(idx,"")
+        except sqlite3.IntegrityError, e:
+            self.notif_text.set("ERROR: COULD NOT CREATE ENTRY\n"+str(e))
+
+    def valid(self):
+        date = self.date.get()
+        if not re.match("\d{4}-\d{2}-\d{2} \d{2}:\d{2}",date):
+            self.notif_text.set("Date format must be YYYY-MM-DD hh:mm")
+            return False
+
+
+        return True
 
     def edit_row(self, rowindex):
         pass
