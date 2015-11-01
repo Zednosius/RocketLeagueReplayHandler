741d5dce01d470ced6371a24a8c2ad7df69e4393
Erik Söderberg <Zednosius@gmail.com>
2015-11-01 23:45:20 +0100

    Added so that it's possible to call 'run_app.py restore' to get your replays back to original state

--- run_app.py
+++ run_app.py
@@ -7,37 +7,59 @@ import ttk
 import gui.HandlerApplication as cst
 import sys
 import os
+import rl_paths
+import shutil
+
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 def output():
     print "Not implemented"
 
-def on_exit():
+def on_exit(rman,root):
     rman.save()
     root.destroy()
-if not os.path.isfile(__location__+"\\rocketleague.db"):
-    import setup
-    setup.initdb()
 
-root = Tk()
-root.title("Rocket League Replay Handler")
-root.minsize(700,500)
-menu = Menu(root)
-root.config(menu=menu)
+def restore():
+    for f in os.listdir(rl_paths.backup_folder()):
+        f = os.path.splitext(f)[0]
+        shutil.move(rl_paths.backup_folder(f),rl_paths.demo_folder(f))
 
-filemenu = Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=output)
-filemenu.add_command(label="Open...", command=output)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=output)
+    shutil.rmtree(rl_paths.tracked_folder())
+    shutil.rmtree(rl_paths.untracked_folder())
 
-helpmenu = Menu(menu)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=output)
 
-rman = cst.ReplayManager(root)
-rman.pack(expand=True,fill=BOTH)
-root.protocol("WM_DELETE_WINDOW",on_exit)
-root.mainloop()
+def main():
+    if not os.path.isfile(__location__+"\\rocketleague.db"):
+        import setup
+        setup.initdb()
+
+    root = Tk()
+    root.title("Rocket League Replay Handler")
+    root.minsize(700,500)
+    menu = Menu(root)
+    root.config(menu=menu)
+
+    filemenu = Menu(menu)
+    menu.add_cascade(label="File", menu=filemenu)
+    filemenu.add_command(label="New", command=output)
+    filemenu.add_command(label="Open...", command=output)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=output)
+
+    helpmenu = Menu(menu)
+    menu.add_cascade(label="Help", menu=helpmenu)
+    helpmenu.add_command(label="About...", command=output)
+
+    rman = cst.ReplayManager(root)
+    rman.pack(expand=True,fill=BOTH)
+    root.protocol("WM_DELETE_WINDOW",lambda : on_exit(rman,root))
+    root.mainloop()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        main()
+    elif len(sys.argv) == 2 and sys.argv[1].lower() == "restore":
+        restore()
+
+
