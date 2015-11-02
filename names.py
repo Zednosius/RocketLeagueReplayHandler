a93ce104541daa606dd5f1faa75e11ca8ae540f9
Erik Söderberg <Zednosius@gmail.com>
2015-11-02 16:22:58 +0100

    Added file with internal stadium name to actual name, might get more in future

---
+++ names.py
@@ -0,0 +1,16 @@
+stadiums = {
+    "stadium_p":"DFH Stadium",
+    
+    "park_p":"Beckwith Park",
+    "park_night_p":"Beckwith Park (midnight",
+    "park_rainy_p":"Beckwith Park (stormy)",
+
+    "eurostadium_p":"Mannfield",
+    "eurostadium_rainy_p":"Mannfield (stormy)",
+    
+    "utopiastadium_p":"Utopia Coliseum",
+    "utopiastadium_dusk_p":"Utopia Coliseum (dusk)",
+
+    "trainstation_p":"Urban Central",
+    "trainstation_night_p":"Urban Central (night)",
+}
