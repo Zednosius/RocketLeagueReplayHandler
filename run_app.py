64239864234a1193992d9c500d48665f60e06161
Erik Söderberg <Zednosius@gmail.com>
2015-11-10 16:45:55 +0100

    Wrapped in try to catch and log any unexpected error

--- run_app.py
+++ run_app.py
@@ -11,6 +11,9 @@ import rl_paths
 import shutil
 import imp
 
+import logging
+logger = logging.getLogger("root")
+print logger
 
 
 def main_is_frozen():
@@ -31,40 +34,51 @@ def on_exit(rman,root):
     root.destroy()
 
 def restore():
+    logger.info("Restoring demo folder to its original state")
     for f in os.listdir(rl_paths.backup_folder()):
         f = os.path.splitext(f)[0]
-        shutil.move(rl_paths.backup_folder(f),rl_paths.demo_folder(f))
+        src = rl_paths.backup_folder(f)
+        dst = rl_paths.demo_folder(f)
+        shutil.cop2(src,dst)
+        logger.debug("Copied from %s to %s",src,dst)
 
     shutil.rmtree(rl_paths.tracked_folder())
     shutil.rmtree(rl_paths.untracked_folder())
+    logger.info("Removed tracked and untracked folder")
+    logger.info("Restore Complete!")
 
 __location__ = get_main_dir()
 def main():
-    if not os.path.isfile(__location__+"\\rocketleague.db"):
-        import db_setup
-        db_setup.initdb()
+    try:
+        if not os.path.isfile(__location__+"\\rocketleague.db"):
+            import db_setup
+            db_setup.initdb()
 
-    root = Tk()
-    root.title("Rocket League Replay Handler")
-    root.minsize(700,500)
-    menu = Menu(root)
-    root.config(menu=menu)
+        root = Tk()
+        root.title("Rocket League Replay Handler")
+        root.minsize(700,500)
+        menu = Menu(root)
+        root.config(menu=menu)
 
-    filemenu = Menu(menu)
-    menu.add_cascade(label="File", menu=filemenu)
-    filemenu.add_command(label="New", command=output)
-    filemenu.add_command(label="Open...", command=output)
-    filemenu.add_separator()
-    filemenu.add_command(label="Exit", command=output)
+        filemenu = Menu(menu)
+        #menu.add_cascade(label="File", menu=filemenu)
+        #filemenu.add_command(label="New", command=output)
+        #filemenu.add_command(label="Open...", command=output)
+        filemenu.add_separator()
+       # filemenu.add_command(label="Exit", command=output)
 
-    helpmenu = Menu(menu)
-    menu.add_cascade(label="Help", menu=helpmenu)
-    helpmenu.add_command(label="About...", command=output)
+        helpmenu = Menu(menu)
+        #menu.add_cascade(label="Help", menu=helpmenu)
+        #helpmenu.add_command(label="About...", command=output)
 
-    rman = cst.ReplayManager(root)
-    rman.pack(expand=True,fill=BOTH)
-    root.protocol("WM_DELETE_WINDOW",lambda : on_exit(rman,root))
-    root.mainloop()
+        rman = cst.ReplayManager(root)
+        rman.pack(expand=True,fill=BOTH)
+        root.protocol("WM_DELETE_WINDOW",lambda : on_exit(rman,root))
+        root.mainloop()
+    except Exception, e:
+        logger.error("Encountered uncaught error")
+        logger.error("Error was: %s", e)
+        print e
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
