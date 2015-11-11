20c40f00bcb97575144a6e591b26d4526a1e8ddf
Erik Söderberg <Zednosius@gmail.com>
2015-11-11 01:33:56 +0100

    modified setup for py2exe

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
-import py2exe
-
-setup(console=['run_app.py'])
+import py2exe,sys,os
+sys.argv.append('py2exe')
+setup(
+    options = {'py2exe':{'bundle_files':1,'compressed':True,}},
+    data_files=['C:\\Python27\\tcl\\tcl8.5\\init.tcl'],
+    console=[{'script':'RocketLeagueReplayHandler.py'}],   
+    zipfile = None,
+    )
