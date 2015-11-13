55ad46821d3ee8f2a7db88e1115a5ec773e91b3d
Erik Söderberg <Zednosius@gmail.com>
2015-11-13 13:47:25 +0100

    Added export methods, can convert replay to json

---
+++ impexp/RLExport.py
@@ -0,0 +1,29 @@
+import zipfile
+import json
+import rl_paths
+import logging
+from db_manager import *
+logger = logging.getLogger(__name__)
+
+
+def convertreplays2json(replay_list):
+    converts = []
+    for replay in replay_list:
+        converts.append(convert2json(replay))
+
+def convert2json(replay_vars):
+    data = {}
+    data['filename'] = replay_vars[1]
+    data['name'] = replay_vars[2]
+    data['map'] = replay_vars[3]
+    data['date'] = replay_vars[4]
+    fill_with_db_items(replay_vars[0],data)
+    logger.debug("DATA: ",data)
+    return json.dumps(data)
+
+def fill_with_db_items(ID, data):
+    with DB_Manager() as dmann:
+        data['teams']  = [x[1:] for x in dmann.get_all_where("teams",id=("=",ID))]
+        data['tags']   = [x[1:] for x in dmann.get_all_where("tags",id=("=",ID)) ]
+        data['notes']  = [x[1:] for x in dmann.get_all_where("notes",id=("=",ID))]
+        data['groups'] = dmann.get_groups(ID)
