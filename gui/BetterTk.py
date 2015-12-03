ffe17f133cc185df91a5e00ecf0601335059246a
Erik Söderberg <Zednosius@gmail.com>
2015-12-03 19:17:00 +0100

    Added a recursive widget bind function

--- gui/BetterTk.py
+++ gui/BetterTk.py
@@ -5,7 +5,14 @@
 
 import Tkinter as tk
 
-
+def recursive_widget_bind(widget,binding,bind_func):
+    """
+    Binds bind_func with binding to widget and all of its descendants. Useful for when you want a semi-wide application binding. 
+    Eg. want binding in main application but not popup windows.
+    """
+    widget.bind(binding,bind_func)
+    for child in widget.winfo_children():
+        recursive_widget_bind(child,binding,bind_func)
 
 class Entry(tk.Entry):
     def __init__(self,parent=None,**kw):
