f896e2ab03e223f7f84cdde296a5e7b8c179208a
Erik Söderberg <Zednosius@gmail.com>
2015-12-04 11:55:42 +0100

    made buildscript better, needs cygwin zip

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
 pyinstaller --onefile --windowed RocketLeagueReplayHandler.py
+cp Updates.txt dist/Updates.txt
+cp log.config dist/log.config
+cd dist
+zip RLRH_v%1-alpha.zip log.config LICENSE RocketLeagueReplayHandler.exe Updates.txt
+cd ..
